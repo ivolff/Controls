bca41c2612d9f22dab02fd0ddc8866b8d4d65929
ivolff <46387375+ivolff@users.noreply.github.com>
2026-06-24 22:06:18 +0300

    Initial commit

---
+++ main/gray_square.go
@@ -0,0 +1,20 @@
+components {
+  id: "sprite"
+  component: "/main/gray_square.sprite"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale3 {
+    x: 1.0
+    y: 1.0
+    z: 1.0
+  }
+}
